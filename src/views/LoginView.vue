00c52c3ea4a285d57facda567e8ab1907e0fbf9e
Francis Lavazelli <francis.lavazelli@gmail.com>
2025-04-21 19:21:16 -0400

    added email verification in login error message

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -92,7 +92,7 @@ const handleLogin = async () => {
     })
     router.push(referer)
   } catch (error) {
-    errorMessage.value = error.response?.data?.message || 'Something went wrong. Please try again.'
+    errorMessage.value = error.response?.data?.message || "Something went wrong. Please try again. If it's your first time logging in, please check your email for verification."
     posthog.capture('user failed to login', {
       email: email,
     })
